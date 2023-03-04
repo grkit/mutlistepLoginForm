f5d9a02c19f0b22c28ccb4fa6012f5fc95057077
Gurkirat <grkit@Gurkirats-MacBook-Air.local>
2023-03-04 11:40:07 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
 import './App.css';
+import {Steps, Form, Input, Button} from "antd"
+import {LoginOutlined, CheckCircleOutlined, ProfileOutlined } from "@ant-design/icons"
+import { useState } from 'react';
 
 function App() {
+
+  const [current, setCurrent] = useState(0)
+  const [loginDetails, setLoginDetails] =useState(null)
+  const [profileDetails, setProfileDetails] = useState(null)
+
+  const onFinishLoginForm=(values)=>{
+    setLoginDetails(values);
+    setCurrent(1)
+  }
+  
+  const onFinishProfileForm=(values)=>{
+    setProfileDetails(values);
+    setCurrent(2)
+  }
+
+  const forms = [
+    <LoginForm onFinish={onFinishLoginForm} initialvalues={loginDetails}className='login'/>,
+    <ProfileForm onFinish={onFinishProfileForm} initialvalues={profileDetails}className='login'/>,
+    <Finish />,
+  ];
+
+  const isStepDisable=(stepNumber)=>{
+    if(stepNumber === 0){
+      return false
+  }
+  if(stepNumber === 1){
+    return loginDetails === null
+  }
+
+  if(stepNumber === 2){
+    return loginDetails === null || profileDetails === null 
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Steps 
+      style={{padding:"32px 16px"}}
+        onChange={setCurrent}
+        current = {current}
+      >
+        <Steps.Step 
+          disabled={isStepDisable(0)} 
+          title="Login" icon={<LoginOutlined/>}>
+         </Steps.Step>
+        <Steps.Step 
+          disabled={isStepDisable(1)} 
+          title="Profile" icon={<ProfileOutlined/>}>
+        </Steps.Step>
+        <Steps.Step 
+          disabled={isStepDisable(2)}
+          title="Finish" 
+          icon={<CheckCircleOutlined/>}>
+        </Steps.Step>
+      </Steps>
+      {forms[current]}
     </div>
-  );
+  )
+};
+
+function LoginForm({onFinish, initialvalues}){
+  return (
+  <Form onFinish={onFinish} initialvalues={initialvalues}>
+    <Form.Item label ='Email' name={'emailAdress'} 
+      rules={[
+        {
+          required:true, 
+          type:'email', 
+          message:'Please enter a valid email'
+        }
+      ]}>
+      <Input/>
+    </Form.Item>
+    <Form.Item label ='Password' name={'password'}
+    rules={[
+      {
+        required:true, 
+        type:'password', 
+        message:'Please enter your password'
+        }
+    ]}>
+      <Input.Password/>
+    </Form.Item>
+    <Button type='primary' htmlType='submit'>Continue</Button>
+  </Form>
+  )
 }
+
+function ProfileForm({onFinish, initialvalues}){
+  return (
+<Form className='form' onFinish={onFinish} initialvalues= {initialvalues}>
+    <Form.Item label ='First Name' name={'firstname'} 
+      rules={[
+        {
+          required:true, 
+          message:'Please enter your first name'
+          }
+      ]}>
+      <Input/>
+    </Form.Item>
+    <Form.Item label ='Last Name' name={'lastname'}
+    rules={[
+      {
+        required:true, 
+        message:'Please enter your last name'
+        }
+    ]}>
+      <Input/>
+    </Form.Item>
+
+    <Button type='primary' htmlType='submit'>
+      Continue
+    </Button>
+  </Form>
+  )
+}
+
+  function Finish (){
+    return <>
+    <h1>You are all set!</h1>
+    <Button type='primary'>Finish</Button>
+    </>
+  }
 
 export default App;
---
+++ src/firebase.confid.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtxfyXfyWL1p1gNGVW_N-39Eo9rB35o-Y",
+  authDomain: "logintest-482f9.firebaseapp.com",
+  projectId: "logintest-482f9",
+  storageBucket: "logintest-482f9.appspot.com",
+  messagingSenderId: "66390170192",
+  appId: "1:66390170192:web:9e44833304091a8970cf55",
+  measurementId: "G-P266X9MHKG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
